a5df50c8b8bee1d52e448d7cb8ab9cab9170751c
Vitória Freitas <freitasvitoria712@gmail.com>
2022-11-30 15:18:55 -0300

    [add] exercicios

--- aula8.js
+++ aula8.js
@@ -11,3 +11,12 @@ const hasDriversLicense2 = false; // A
 const hasGoodVision2 = true; // B
 
 console.log(hasDriversLicense2 && hasGoodVision2);
+
+const isTired = false; // C
+console.log(hasDriversLicense2 && hasGoodVision2 && isTired);
+
+if (hasDriversLicense2 && hasGoodVision2 && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
