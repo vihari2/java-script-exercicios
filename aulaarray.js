6fa72b92c478ca76af12c93d970001b903dc85a6
vitoria f <freitasvitoria712@gmail.com>
2022-12-07 17:39:41 -0300

    [add] aula de array

---
+++ aulaarray.js
@@ -0,0 +1,12 @@
+const friends = ["Jessica", "Vitória", "William", "Bruno"];
+
+console.log(friends);
+console.log(friends.length);
+console.log(friends.length - 1);
+
+const firstName = "Jonas";
+const jonas = [firstName, "Carla", 2022 - 1998, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//não pode colocar uma array como argumento de uma função, só pode por em modo variavel = umaFunção(array[0])
