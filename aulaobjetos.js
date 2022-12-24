c21bedf46089d6c37a6eeb3abfa89c1f6e92723a
vitoria <vitoria.dev.f@gmail.com>
2022-12-23 22:19:28 -0300

    [add] aula de objetos

---
+++ aulaobjetos.js
@@ -0,0 +1,24 @@
+// Array
+
+const vitoriaArray = [
+  "Vitoria",
+  "Silva",
+  2022 - 1997,
+  "student",
+  ["Maria", "Marco", "Alexander"],
+];
+
+// Objects have 5 keys and every key has a value
+// Esse objeto chamado vitória tem 5 propriedades.
+//Usamos objetos para agrupar diferentes variáveis que realmente tem alguma relação entre si.
+const vitoria = {
+  firstName: "Vitoria",
+  lastName: "Silva",
+  Age: 2022 - 1997,
+  job: "student",
+  friends: ["Maria", "Marco", "Alexander"],
+};
+
+//diferença entre objetos e arrays
+//objetos=ordem desses valores não importa quando queremos recuperar um valor específico
+//arrays=ordem desses valores importa quando queremos recuperar um valor específico
