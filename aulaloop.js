0dae88448f09be43b474109876c73b5c3198fc75
vitoria <vitoria.dev.f@gmail.com>
2022-12-27 12:21:35 -0300

    [add] for loop aula

---
+++ aulaloop.js
@@ -0,0 +1,12 @@
+//automizar tarefas repetitivas
+//FOR LOOP = CONTADOR
+//for loop keeps running while condition is TRUE
+//tem 3 partes (valor inicial de um contador,  a segunda parte é uma condição lógica que é avaliada antes de cada iteração de loop,
+//se essa condição lógica é verdadeira, então o loop continua, se for falsa, o loop termina.
+//a terceira parte atualiza o contador a cada iteração do loop)
+
+for (let rep = 1; rep <= 10; rep = rep + 1) {
+  console.log(`lifting weights repetition ${rep} 🏃‍♂️`);
+}
+
+//rep++ é a mesma coisa que rep = rep + 1
