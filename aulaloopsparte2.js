bde815246b4d8ee5ba16d2a313b7ffbafa94900c
vitoria <vitoria.dev.f@gmail.com>
2022-12-27 13:09:36 -0300

    [add] loop aula

---
+++ aulaloopsparte2.js
@@ -0,0 +1,11 @@
+const vitoriaArray = [
+  "Vitoria",
+  "Freitas",
+  "1997",
+  "student",
+  ["Lucas", "Bruna", "Camila"],
+];
+///variavel contador = i, é uma variavel que vai ser usada para contar quantas vezes o loop vai rodar.
+for (let i = 0; i < 5; i++) {
+  console.log(vitoriaArray[i]);
+}
