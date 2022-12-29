b5497cb07cf31a9d79da5134d0643462ec3e8c48
vitoria <vitoria.dev.f@gmail.com>
2022-12-29 18:41:28 -0300

    [add] aula  loops inside of loop

--- loopsinloopsparte2.js
+++ loopsinloopsparte2.js
@@ -1,3 +1,8 @@
+// For loop inside a for loop
 for (let exercise = 1; exercise < 4; exercise++) {
-  console.log(`--------------Starting exercise ${exercise}`);
+  console.log(`--------------Starting exercise ${exercise}-----------------`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
+  }
 }
