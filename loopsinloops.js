1ed4071c352f8d685ae88d558d4a293b8a186950
vitoria <vitoria.dev.f@gmail.com>
2022-12-29 17:55:49 -0300

    [add] loops de trás pra frente

---
+++ loopsinloops.js
@@ -0,0 +1,14 @@
+//looping backwards
+const vitoriaArray = [
+  "Vitoria",
+  "Freitas",
+  1997,
+  "student",
+  ["Lucas", "Bruna", "Camila"],
+];
+
+//4-3-2-1-0
+
+for (let i = vitoriaArray.length - 1; i >= 0; i = i - i) {
+  console.log(vitoriaArray[i]);
+}
